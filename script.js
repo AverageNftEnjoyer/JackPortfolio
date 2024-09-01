e9fc16aa65108d1959df03c269c02d657f0048b3
Jack Pastor <Jackpastor4@me.com>
2024-08-31 21:19:25 -0400

    1st Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript for toggling the hamburger menu
+function toggleMenu() {
+    const menu = document.querySelector('.hamburger-menu');
+    menu.classList.toggle('active');
+  }
+  
+  
