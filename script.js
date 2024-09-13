b82961585ee55fc8a6d674d8826aa1dd1e3d6cfe
Jack Pastor <Jackpastor4@me.com>
2024-09-12 23:58:06 -0400

    Mobile Hotfix

--- script.js
+++ script.js
@@ -57,3 +57,25 @@ document.addEventListener("DOMContentLoaded", function () {
   typeMessage();
 });
 
+function typingEffect() {
+  const text = 'Based in Pittsburgh, PA';
+  const speed = 80; // Typing speed
+  const typingElement = document.getElementById('typing-text');
+  let index = 0;
+
+  function type() {
+      if (index < text.length) {
+          typingElement.textContent += text.charAt(index);
+          index++;
+          setTimeout(type, speed);
+      }
+  }
+
+  // Start the typing effect
+  type();
+}
+
+// Initialize the typing effect when the page is loaded
+document.addEventListener('DOMContentLoaded', function() {
+  typingEffect(); // Start typing effect
+});
