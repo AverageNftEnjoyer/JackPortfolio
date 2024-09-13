8479447d5615fd2ca9f62b29b86c27dfbd00615d
Jack Pastor <Jackpastor4@me.com>
2024-09-12 20:45:30 -0400

    git push MobileHotfix

--- mobileicons.js
+++ mobileicons.js
@@ -8,12 +8,17 @@ function rotateIcons() {
     const textRect = pittsburghText.getBoundingClientRect(); // Get the Pittsburgh, PA text dimensions
 
     floatingIcons.forEach(icon => {
-        let posX = Math.random() * containerRect.width; // Random starting position for each icon
+        // Increase the size of the icons
+        icon.style.width = '80px'; // Set the width to 80px
+        icon.style.height = '80px'; // Set the height to 80px
+
+        let posX = -200 + Math.random() * containerRect.width; // Start icons far off to the left (-200) with random positioning
         let speed = 1 + Math.random() * 2; // Random speed for each icon between 1 and 3
 
-        // Position the icon horizontally
+        // Position the icon horizontally and below the Pittsburgh, PA text
         icon.style.position = 'absolute';
-        icon.style.top = `${textRect.bottom + 20}px`; // Position the icons 20px below the "Pittsburgh, PA" text
+        icon.style.top = `${textRect.bottom + 40}px`; // Position the icons 40px below the "Pittsburgh, PA" text
+        icon.style.left = `${posX}px`; // Add margin to the left to start them far off screen
 
         function animateIcon() {
             // Move the icon to the right
@@ -21,7 +26,7 @@ function rotateIcons() {
 
             // If the icon moves off the right side of the screen, reset it to the left
             if (posX > containerRect.width) {
-                posX = -icon.offsetWidth; // Move it just off the left edge of the screen
+                posX = -icon.offsetWidth - 200; // Move it far off the left edge of the screen (-200)
             }
 
             // Apply the transformation
